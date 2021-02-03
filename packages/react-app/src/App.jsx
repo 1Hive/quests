a768c3faaea24e54dc3010ba1d865e3b55c33ea6
Corantin Noll <corantin.noll.1@etsmtl.net>
2021-02-03 04:02:28 -0500

    Restore Faucet in main

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -202,6 +202,46 @@ export default function App(props) {
           </Switch>
         </Content>
         <Footer style={{ textAlign: 'center' }}>Honey Quest @2021 Founded by <a href="https://1hive.org/">1Hive</a></Footer>
+        {/* 🗺 Extra UI like gas price, eth price, faucet, and support: */}
+        <div style={{ position: "fixed", textAlign: "left", left: 0, bottom: 20, padding: 10 }}>
+          <Row align="middle" gutter={[4, 4]}>
+            <Col span={8}>
+              <Ramp price={price} address={address} />
+            </Col>
+
+            <Col span={8} style={{ textAlign: "center", opacity: 0.8 }}>
+              <GasGauge gasPrice={gasPrice} />
+            </Col>
+            <Col span={8} style={{ textAlign: "center", opacity: 1 }}>
+              <Button
+                onClick={() => {
+                  window.open("https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA");
+                }}
+                size="large"
+                shape="round"
+              >
+                <span style={{ marginRight: 8 }} role="img" aria-label="support">
+                  💬
+              </span>
+              Support
+            </Button>
+            </Col>
+          </Row>
+
+          <Row align="middle" gutter={[4, 4]}>
+            <Col span={24}>
+              {
+
+                /*  if the local provider has a signer, let's show the faucet:  */
+                localProvider && localProvider.connection && localProvider.connection.url && localProvider.connection.url.indexOf(window.location.hostname) >= 0 && !process.env.REACT_APP_PROVIDER && price > 1 ? (
+                  <Faucet localProvider={localProvider} price={price} ensProvider={mainnetProvider} />
+                ) : (
+                    ""
+                  )
+              }
+            </Col>
+          </Row>
+        </div>
       </Layout>
       <GodTool localProvider={localProvider} mainnetProvider={mainnetProvider} price={price} address={address} gasPrice={gasPrice} />
     </BrowserRouter>
