6a5615e5be8f3631cc5d82964bf791697933618d
FrederikThebergeAguilar <frederik.theberge-aguilar.1@etsmtl.net>
2021-03-22 17:38:38 -0400

    aragon

--- packages/hardhat/contracts/QuestFactory.sol
+++ packages/hardhat/contracts/QuestFactory.sol
@@ -4,29 +4,38 @@ pragma solidity >=0.6.0 <0.8.0;
 contract QuestFactory {
     event QuestCreated(address questAddress, string _content);
 
-    function createQuest(string calldata _content) external {
-        require(msg.sender == aragonGovernAddress, "Error: sender not govern");
-        Quest quest = new Quest(_content);
+    function createQuest(string calldata _content,uint256 _terminationDate) external {
+        Quest quest = new Quest(_content,_terminationDate);
         emit QuestCreated(address(quest), _content);
     }
+
 }
 
 contract Quest {
     string private userFiles;
     string public content;
+    address payable private aragonGovernAddress;
+    uint256 public terminationDate;
 
-    constructor(string memory _content) public {
+    constructor(string memory _content,uint256 _terminationDate) public {
         content = _content;
+        terminationDate = _terminationDate;
     }
 
     function claim(
-        require(msg.sender == aragonGovernAddress, "Error: sender not govern");
         address payable player,
         uint256 amount,
         string calldata file
     ) external payable {
         require(bytes(file).length != 0, "You must join a file");
-        userFiles = file;
-        player.transfer(amount);
+        require(msg.sender == aragonGovernAddress, "Error: sender not govern");
+
+        if(terminationDate < block.timestamp){
+            aragonGovernAddress.transfer(address(this).balance);
+        } else {
+            userFiles = file;
+            player.transfer(amount);
+        }
     }
+
 }
