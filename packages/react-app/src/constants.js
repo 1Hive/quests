fdf32ebdfd9ddfede0aae5b9be5005b6ac753aa3
Corantin Noll <corantin.noll@gmail.com>
2021-04-27 12:40:41 -0400

    Change title and description from Honey Quest to Quest

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 export const PCT_BASE = BigInt(1e18);
 
-export const APP_TITLE = 'Honey Quest';
+export const APP_TITLE = 'Quest';
 
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
 export const INFURA_ID = '460f40a260564ac4a4f4b3fffb032dad';
