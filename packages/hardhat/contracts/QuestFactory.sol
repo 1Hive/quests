436cf1f31ddb0a69ed8f52d881d723eecd611aca
William Griffiths <willjgriff@gmail.com>
2021-07-08 10:40:27 +0100

    Tidy up contracts.

--- packages/hardhat/contracts/QuestFactory.sol
+++ packages/hardhat/contracts/QuestFactory.sol
@@ -1,93 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./Quest.sol";
+
 
 contract QuestFactory {
-    event QuestCreated(address questAddress, string _content);
+
     address public aragonGovernAddress;
+
+    event QuestCreated(address questAddress, bytes _requirements);
 
     constructor(address _aragonGovernAddress) {
         aragonGovernAddress = _aragonGovernAddress;
     }
 
     function createQuest(
-        string calldata _content,
-        uint256 _terminationDate,
-        address _fallbackAddress
-    ) external {
-        Quest quest =
-            new Quest(
-                _content,
-                _terminationDate,
-                aragonGovernAddress,
-                _fallbackAddress
-            );
-        emit QuestCreated(address(quest), _content);
-    }
-}
-
-contract Quest {
-    event QuestClaimed(bytes file, address player, uint256 amount);
-
-    struct Claim {
-        bytes file;
-        address player;
-        uint256 amount;
-    }
-
-    Claim[] public claims;
-    string public userFiles;
-    string public content;
-    address public aragonGovernAddress;
-    address public fallbackAddress;
-    uint256 public terminationDate;
-    IERC20 public token;
-
-    constructor(
-        string memory _content,
-        uint256 _terminationDate,
-        address _aragonGovernAddress,
-        address _fallbackAddress
-    ) {
-        content = _content;
-        terminationDate = _terminationDate;
-        aragonGovernAddress = _aragonGovernAddress;
-        fallbackAddress = _fallbackAddress;
-    }
-
-    function returnFunds() external {
-        require(terminationDate < block.timestamp, "Quest is not expired yet.");
-        token.transfer(fallbackAddress, token.totalSupply());
-    }
-
-    function recoverNativeTokens() external {
-        token.transfer(fallbackAddress, token.totalSupply());
-    }
-
-    function claim(
-        bytes memory file,
-        address player,
-        uint256 amount
-    ) external {
-        require(msg.sender == aragonGovernAddress, "Error: sender not govern");
-        require(bytes(file).length != 0, "You must join a file");
-
-        if (amount > 0) {
-            require(
-                token.transfer(player, amount),
-                "Could not send tokens to the buyer"
-            );
-        } else if (amount == 0) {
-            require(
-                token.transfer(player, token.balanceOf(address(this))),
-                "Could not send tokens to the buyer"
-            );
-        }
-
-        claims.push(Claim(file, player, amount));
-
-        emit QuestClaimed(file, player, amount);
-        //Send to the aragon quest stack
+        bytes memory _requirements,
+        IERC20 _rewardToken,
+        uint256 _expireTime,
+        address payable _fundsRecoveryAddress
+    )
+        external
+    {
+        Quest quest = new Quest(_requirements, _rewardToken, _expireTime, aragonGovernAddress, _fundsRecoveryAddress);
+        emit QuestCreated(address(quest), _requirements);
     }
 }
