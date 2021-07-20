1bd917955da6743b075dd07c3e3848ad5617b32f
Corantin Noll <corantin.noll@gmail.com>
2021-07-20 18:37:10 -0400

    Change packages

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -5,6 +5,7 @@ require("@nomiclabs/hardhat-web3");
 require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 require("solidity-coverage");
+require("hardhat-deploy");
 
 const fs = require("fs");
 const chalk = require("chalk");
