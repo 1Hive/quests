2d2d7f60066630096cc821296ba29fa2fe240828
Alex <alex0394@hotmail.com>
2021-12-27 15:41:33 -0500

    use isEdit with gu16

--- packages/react-app/src/components/shared/quest.tsx
+++ packages/react-app/src/components/shared/quest.tsx
@@ -182,7 +182,7 @@ export default function Quest({
           </Outset>
         }
         secondary={
-          <Outset gu16={questMode !== QUEST_MODE.Create && questMode !== QUEST_MODE.Update}>
+          <Outset gu16={!isEdit}>
             {bounty !== null && (
               <AmountFieldInputFormik
                 id="bounty"
