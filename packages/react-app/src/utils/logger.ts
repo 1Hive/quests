43cb4ad04267b5d3fff52f9c2f930faa4a180f45
Corantin <corantin.noll@gmail.com>
2022-01-25 17:24:19 -0500

    Versioning

--- packages/react-app/src/utils/logger.ts
+++ packages/react-app/src/utils/logger.ts
@@ -4,19 +4,18 @@ import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
 import { noop } from 'lodash';
 import env from 'src/environment';
-import { getNetwork } from 'src/networks';
 
-const { name } = getNetwork();
+const { version } = require('../../package.json');
 
 Sentry.init({
-  environment: `${process.env.NODE_ENV}-${name}`,
+  environment: `${process.env.NODE_ENV}-chain${env('CHAIN_ID')}`,
   dsn: env('SENTRY_DSN_URI'),
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
   tracesSampleRate: 0,
   integrations: [new Integrations.BrowserTracing()],
-  release: 'v0.1.0-alpha',
+  release: version,
   autoSessionTracking: false, // default: true
 });
 
