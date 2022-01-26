828a74cca4a75bf0cf25f612a2119815f860d9d9
Corantin <corantin.noll@gmail.com>
2022-01-26 16:57:30 -0500

    Update footer.tsx

--- packages/react-app/src/components/footer.tsx
+++ packages/react-app/src/components/footer.tsx
@@ -8,19 +8,15 @@ const FooterContainerStyled = styled.div`
   margin: auto;
   background-color: ${(props: any) => props.background};
   box-shadow: rgb(0 0 0 / 5%) 3px -2px 0px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
 
   a {
     color: ${({ color }: any) => color} !important;
   }
 
   padding: ${GUpx(5)} ${GUpx(3)};
-`;
-
-const FooterWrapperStyled = styled.div`
-  width: fit-content;
-  display: flex;
-  flex-wrap: wrap;
-  column-gap: 150px;
 `;
 
 const FooterColumnStyled = styled.div`
@@ -49,47 +45,45 @@ export default function footer() {
   const theme = useTheme();
   return (
     <FooterContainerStyled background={theme.surface} color={theme.contentSecondary}>
-      <FooterWrapperStyled>
-        <FooterColumnStyled>
-          <FooterTitleStyled>Community</FooterTitleStyled>
-          <FooterLinkStyled href="https://discord.gg/4fm7pgB" external>
-            Discord
-          </FooterLinkStyled>
-          <FooterLinkStyled href="https://github.com/1Hive" external>
-            Github
-          </FooterLinkStyled>
-          <FooterLinkStyled href="https://twitter.com/1HiveOrg" external>
-            Twitter
-          </FooterLinkStyled>
-          <FooterLinkStyled href="https://t.me/honeyswapdex" external>
-            Telegram
-          </FooterLinkStyled>
-          <FooterLinkStyled href="https://forum.1hive.org/" external>
-            Forum
-          </FooterLinkStyled>
-        </FooterColumnStyled>
-        <FooterColumnStyled>
-          <FooterTitleStyled>Documentation</FooterTitleStyled>
-          <FooterLinkStyled href="https://wiki.1hive.org/projects/quests" external>
-            Wiki
-          </FooterLinkStyled>
-        </FooterColumnStyled>
-        <FooterColumnStyled>
-          <FooterTitleStyled>Feedback</FooterTitleStyled>
-          <FooterLinkStyled
-            href="https://github.com/1Hive/quests/issues/new?assignees=&labels=Feature&template=feature----feature_title-.md"
-            external
-          >
-            Submit an idea
-          </FooterLinkStyled>
-          <FooterLinkStyled
-            href="https://github.com/1Hive/quests/issues/new?assignees=&labels=&template=bug----bug_title-.md"
-            external
-          >
-            Report a bug
-          </FooterLinkStyled>
-        </FooterColumnStyled>
-      </FooterWrapperStyled>
+      <FooterColumnStyled>
+        <FooterTitleStyled>Community</FooterTitleStyled>
+        <FooterLinkStyled href="https://discord.gg/4fm7pgB" external>
+          Discord
+        </FooterLinkStyled>
+        <FooterLinkStyled href="https://github.com/1Hive" external>
+          Github
+        </FooterLinkStyled>
+        <FooterLinkStyled href="https://twitter.com/1HiveOrg" external>
+          Twitter
+        </FooterLinkStyled>
+        <FooterLinkStyled href="https://t.me/honeyswapdex" external>
+          Telegram
+        </FooterLinkStyled>
+        <FooterLinkStyled href="https://forum.1hive.org/" external>
+          Forum
+        </FooterLinkStyled>
+      </FooterColumnStyled>
+      <FooterColumnStyled>
+        <FooterTitleStyled>Documentation</FooterTitleStyled>
+        <FooterLinkStyled href="https://wiki.1hive.org/projects/quests" external>
+          Wiki
+        </FooterLinkStyled>
+      </FooterColumnStyled>
+      <FooterColumnStyled>
+        <FooterTitleStyled>Feedback</FooterTitleStyled>
+        <FooterLinkStyled
+          href="https://github.com/1Hive/quests/issues/new?assignees=&labels=Feature&template=feature----feature_title-.md"
+          external
+        >
+          Submit an idea
+        </FooterLinkStyled>
+        <FooterLinkStyled
+          href="https://github.com/1Hive/quests/issues/new?assignees=&labels=&template=bug----bug_title-.md"
+          external
+        >
+          Report a bug
+        </FooterLinkStyled>
+      </FooterColumnStyled>
     </FooterContainerStyled>
   );
 }
