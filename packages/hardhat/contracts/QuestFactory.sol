e5f42716494306ae1ded462ec668f37f6bad0b9a
Corantin Noll <corantin.noll@gmail.com>
2022-04-29 04:19:47 -0400

    Remove console log

--- packages/hardhat/contracts/QuestFactory.sol
+++ packages/hardhat/contracts/QuestFactory.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./libraries/Deposit.sol";
 import "./libraries/Models.sol";
-import "hardhat/console.sol";
 import "./Quest.sol";
 
 contract QuestFactory is Ownable {
@@ -75,8 +74,6 @@ contract QuestFactory is Ownable {
             address(deposit.token),
             deposit.amount
         );
-
-        console.log(address(quest));
 
         return address(quest);
     }
