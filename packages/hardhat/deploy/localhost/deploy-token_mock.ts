872bb451d52fca4bda962bfaecdbc8856a5125fd
Corantin Noll <corantin.noll@gmail.com>
2022-05-01 23:54:23 -0400

    Rinkeby deploiement

--- packages/hardhat/deploy/localhost/deploy-token_mock.ts
+++
@@ -1,9 +0,0 @@
-module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
-  await deploy("TokenMock", {
-    from: deployer,
-    log: true,
-  });
-};
-module.exports.tags = ["TokenMock"];
