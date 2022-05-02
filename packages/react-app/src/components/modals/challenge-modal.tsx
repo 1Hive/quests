b9e6779458ccb42f8d5e05c8b79d5d0d3a9d6687
cryptoguru1012 <tiop08317@gmail.com>
2022-05-02 05:50:32 -0400

    update for challenge

--- packages/react-app/src/components/modals/challenge-modal.tsx
+++ packages/react-app/src/components/modals/challenge-modal.tsx
@@ -56,17 +56,27 @@ export default function ChallengeModal({ claim, challengeDeposit, onClose = noop
   const [isFeeDepositSameToken, setIsFeeDepositSameToken] = useState<boolean>();
   const [challengeFee, setChallengeFee] = useState<TokenAmountModel | undefined>(undefined);
   const [isFormValid, setIsFormValid] = useState(false);
-  const { setTransaction } = useTransactionContext();
+  const { transaction, setTransaction } = useTransactionContext();
   const formRef = useRef<HTMLFormElement>(null);
   const { walletAddress } = useWallet();
 
+  const fetchFee = async () => {
+    const feeAmount = await QuestService.fetchChallengeFee();
+    if (feeAmount) setChallengeFee(feeAmount);
+  };
+
   useEffect(() => {
-    const fetchFee = async () => {
-      const feeAmount = await QuestService.fetchChallengeFee();
-      if (feeAmount) setChallengeFee(feeAmount);
-    };
     fetchFee();
   }, [walletAddress]);
+
+  useEffect(() => {
+    if (
+      transaction?.status === ENUM_TRANSACTION_STATUS.Confirmed &&
+      transaction?.transactionType === 'ClaimChallenge'
+    ) {
+      fetchFee();
+    }
+  }, [transaction]);
 
   useEffect(() => {
     let handle: number;
