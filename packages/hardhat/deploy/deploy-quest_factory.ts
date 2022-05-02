a68094ee589fcca29b93979038e730414879f093
Corantin <corantin.noll@gmail.com>
2022-05-01 23:50:38 -0400

    .emailHardhat deploy update

--- packages/hardhat/deploy/deploy-quest_factory.ts
+++ packages/hardhat/deploy/deploy-quest_factory.ts
@@ -3,7 +3,11 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deployer, govern } = await getNamedAccounts();
   await deploy("QuestFactory", {
     from: deployer,
-    args: [govern],
+    args: [
+      govern,
+      "0x6e7c3BC98bee14302AA2A98B4c5C86b13eB4b6Cd",
+      "100000000000000000",
+    ],
     log: true,
   });
 };
