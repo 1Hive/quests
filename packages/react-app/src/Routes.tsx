bcdda5b3594995832072e795e5c7203439bde03e
Corantin <corantin.noll@gmail.com>
2022-07-12 12:24:48 -0400

    Revert Browser router + add chain specificity  in evidence ipfs

--- packages/react-app/src/Routes.tsx
+++ packages/react-app/src/Routes.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import QuestDetail from './components/views/quest-detail';
 import QuestList from './components/views/quest-list';
 import { ENUM_PAGES } from './constants';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
+    <>
       <Switch>
         <Redirect exact from={`/${ENUM_PAGES.List}`} to="/home" />
         <Route path="/home" component={QuestList} />
         <Route path={`/${ENUM_PAGES.Detail}`} component={QuestDetail} />
         <Redirect to="/home" />
       </Switch>
-    </BrowserRouter>
+    </>
   );
 }
