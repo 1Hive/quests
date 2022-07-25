78fdfa28aacce7bfa854cbbcd825b92ce55e37d6
Corantin <corantin.noll@gmail.com>
2022-07-25 11:58:46 -0400

    Cut noise of Using STAGIN=true when activating verbose in console

--- packages/react-app/src/utils/logger.ts
+++ packages/react-app/src/utils/logger.ts
@@ -1,22 +1,23 @@
 /* eslint-disable no-console */
 import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
+// import { Integrations } from '@sentry/tracing';
 import { noop } from 'lodash';
-import env from 'src/environment';
+// import env from 'src/environment';
 
-const { version } = require('../../package.json');
+// const { version } = require('../../package.json');
 
-Sentry.init({
-  environment: `${process.env.NODE_ENV}-chain-${env('CHAIN_ID')}`,
-  dsn: env('SENTRY_DSN_URI'),
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 0,
-  integrations: [new Integrations.BrowserTracing()],
-  release: version,
-  autoSessionTracking: false, // default: true
-});
+// TODO : Restore when 1hive allows it
+// Sentry.init({
+//   environment: `${process.env.NODE_ENV}-chain-${env('CHAIN_ID')}`,
+//   dsn: env('SENTRY_DSN_URI'),
+//   // Set tracesSampleRate to 1.0 to capture 100%
+//   // of transactions for performance monitoring.
+//   // We recommend adjusting this value in production
+//   tracesSampleRate: 0,
+//   integrations: [new Integrations.BrowserTracing()],
+//   release: version,
+//   autoSessionTracking: false, // default: true
+// });
 
 Sentry.configureScope((scope) => {
   if (process.env.NODE_ENV === 'production') scope.setLevel(Sentry.Severity.Error);
