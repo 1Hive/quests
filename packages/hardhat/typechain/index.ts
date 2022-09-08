b32c0af44e46fb7a51fe1f3653dac02301e05d54
Corantin <corantin.noll@gmail.com>
2022-09-08 00:35:18 -0400

    Update index.ts

--- packages/hardhat/typechain/index.ts
+++ packages/hardhat/typechain/index.ts
@@ -2,7 +2,6 @@
 /* tslint:disable */
 /* eslint-disable */
 export type { Ownable } from "./Ownable";
-export type { ERC20 } from "./ERC20";
 export type { IERC20Permit } from "./IERC20Permit";
 export type { IERC20Metadata } from "./IERC20Metadata";
 export type { IERC20 } from "./IERC20";
@@ -14,7 +13,6 @@ export type { Quest } from "./Quest";
 export type { QuestFactory } from "./QuestFactory";
 
 export { Ownable__factory } from "./factories/Ownable__factory";
-export { ERC20__factory } from "./factories/ERC20__factory";
 export { IERC20Permit__factory } from "./factories/IERC20Permit__factory";
 export { IERC20Metadata__factory } from "./factories/IERC20Metadata__factory";
 export { IERC20__factory } from "./factories/IERC20__factory";
