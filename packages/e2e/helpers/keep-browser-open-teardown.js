5f9f588eb30c1946ca7180e29e2e2ddca3500870
Corantin <corantin.noll@gmail.com>
2022-10-10 21:43:07 -0400

    Update keep open

--- packages/e2e/helpers/keep-browser-open-teardown.js
+++ packages/e2e/helpers/keep-browser-open-teardown.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 
 module.exports = async () => {
-  if (!process.env.E2E_KEEP_BROWSER_OPEN) {
+  if (
+    !process.env.E2E_KEEP_BROWSER_OPEN ||
+    process.env.E2E_KEEP_BROWSER_OPEN === 'false'
+  ) {
     await global.browser.close();
   }
 };
