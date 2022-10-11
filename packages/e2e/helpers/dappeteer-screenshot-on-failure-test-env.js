e1048ad8a7ebd921c58ab4c9711b5d206d7bc97a
Corantin <corantin.noll@gmail.com>
2022-10-10 21:48:28 -0400

    Screenshot

--- packages/e2e/helpers/dappeteer-screenshot-on-failure-test-env.js
+++ packages/e2e/helpers/dappeteer-screenshot-on-failure-test-env.js
@@ -34,9 +34,9 @@ class DappeteerEnvironment extends NodeEnvironment {
         fs.mkdirSync(dir);
       }
       this.global.page.screenshot({
-        path: `${dir}/${
-          state.currentlyRunningTest.name
-        }-${new Date().toLocaleTimeString()}.png`,
+        path: `${dir}/${state.currentlyRunningTest.name}-${new Date()
+          .toLocaleTimeString()
+          .replace(' ', '')}.png`,
         type: 'png',
         fullPage: true,
       });
--- packages/e2e/helpers/keep-browser-open-teardown.js
+++ packages/e2e/helpers/keep-browser-open-teardown.js
@@ -5,6 +5,7 @@ module.exports = async () => {
     !process.env.E2E_KEEP_BROWSER_OPEN ||
     process.env.E2E_KEEP_BROWSER_OPEN === 'false'
   ) {
+    await new Promise((res) => setTimeout(res, 1000)); // wait for the browser to close to let time screenshot to be taken
     await global.browser.close();
   }
 };
